51290dccfed256eacac01031c1c575024bfcf885
bd-dev-ismail <ismailratul7@gmail.com>
2023-01-06 07:27:06 +0600

    primary setup & design navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import Home from "./page/Home/Home";
+import { theme } from "./Theme/theme";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      element: <Layout/>,
+      path: '/',
+      children: [
+        {
+          element: <Home/>,
+          path: '/',
+        }
+      ]
+    }
+  ])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <ThemeProvider theme={theme}>
+      <RouterProvider router={router}/>
+      <CssBaseline/>
+     </ThemeProvider>
     </div>
   );
 }
